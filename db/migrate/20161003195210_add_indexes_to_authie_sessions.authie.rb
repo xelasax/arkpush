d6ebd109a589f4f18916a64080869364d63445d7
quame <quamelexiz@gmail.com>
2026-03-22 18:09:44 +0000

    fix(ci): add base migration version to legacy migrations for Rails 7 compatibility

--- db/migrate/20161003195210_add_indexes_to_authie_sessions.authie.rb
+++ db/migrate/20161003195210_add_indexes_to_authie_sessions.authie.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # This migration comes from authie (originally 20141013115205)
-class AddIndexesToAuthieSessions < ActiveRecord::Migration
+class AddIndexesToAuthieSessions < ActiveRecord::Migration[4.2]
 
   def change
   end
